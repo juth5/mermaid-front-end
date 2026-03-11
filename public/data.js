98cbd80d5b808ca561a737dc140be77d09b46718
深川　貴司 <fukagawatakashiee893@gmail.com>
2026-03-12 00:28:45 +0900

    image

--- public/data.js
+++ public/data.js
@@ -1,7 +1,7 @@
 const data = {
   umlTypes: [
-    { value: "sequenceDiagram", label: "シーケンス図", main_prompt: "シーケンス図を書いてください。(autonumberを含めてください)" },
-    { value: "flowchart", label: "フローチャート", main_prompt: "フローチャート図を書いてください。" },
+    { value: "sequenceDiagram", label: "シーケンス図", main_prompt: "シーケンス図を書いてください。(autonumberを含めてください)", description: [ "登場人物（オブジェクト/システム）の役割" ] },
+    { value: "flowchart", label: "フローチャート", main_prompt: "フローチャート図を書いてください。", description: [ "処理フロー図（システム処理フロー）", "業務フロー図", "画面遷移図（UIフロー）"] },
   ],
 };
 export default data;
